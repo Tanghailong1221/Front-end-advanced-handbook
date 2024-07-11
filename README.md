37743396a6737497c213e4b14cdcb7d66072570a
Tanghailong1221 <1936202711@qq.com>
2024-07-12 00:15:28 +0800

    feat:项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# Front-end-advanced-handbook
 前端面试宝典，本项目包括HTML、CSS、JS/TS、VUE、REACT等一些列前端技术点，面试技巧等内容。内容免费，仅作为学习参考。
+
+## 目录
+
+- [目录](#目录)
+  - [前言](#前言)
+  - [面试技巧](#面试技巧)
+  - [推荐书籍](#推荐书籍)
+  - [前端基础](#前端基础)
+  - [前端笔试](#前端笔试)
+  - [前端原理](#前端原理)
+  - [计算机基础](#计算机基础)
+  - [数据结构与算法](#数据结构与算法)
+  - [前端框架](#前端框架)
+  - [框架原理](#框架原理)
+  - [UI组件](#ui组件)
+  - [性能优化](#性能优化)
+  - [工程化](#工程化)
+  - [工程化原理](#工程化原理)
+  - [前端安全](#前端安全)
+- [说明](#说明)
+  - [关于读者](#关于读者)
+  - [关于内容](#关于内容)
+  - [关于后续](#关于后续)
+
+### 前言
+
+* [项目阅读指南](docs/guide/README.md)
+* [为什么会有这个项目](docs/guide/preface.md)
+
+### 面试技巧
+
+* [面试官到底想看什么样的简历？](docs/guide/resume.md)
+* [面试回答问题的技巧](docs/guide/project.md)
+* [如何通过HR面](docs/guide/hr.md)
+
+### 推荐书籍
+
+* [书籍推荐](docs/guide/book.md)
+
+### 前端基础
+
+* [前端基础](docs/guide/htmlBasic.md)
+* [CSS基础](docs/guide/cssBasic.md)
+* [JavaScript基础](docs/guide/jsBasic.md)
+* [浏览器面试题](docs/guide/browser.md)
+* [DOM面试题](docs/guide/dom.md)
+
+### 前端笔试
+
+* [HTTP笔试部分](docs/guide/httpWritten.md)
+* [书籍推荐](docs/guide/book.md)
+
+### 前端原理
+
+* [JavaScript的『预解释』与『变量提升』](docs/guide/hoisting.md)
+* [Event Loop详解](docs/guide/eventLoop.md)
+* [实现不可变数据](docs/guide/immutable.md)
+* [JavaScript内存管理](docs/guide/memory.md)
+* [实现深克隆](docs/guide/deepclone.md)
+* [如何实现一个Event](docs/guide/event.md)
+* [JavaScript的运行机制](docs/guide/mechanism.md)
+
+### 计算机基础
+
+* [HTTP面试题](docs/guide/http.md)
+* [TCP面试题](docs/guide/tcp.md)
+
+### 数据结构与算法
+
+* [算法面试题](docs/guide/algorithm.md)
+
+### 前端框架
+
+* [关于前端框架的面试须知](docs/guide/framework.md)
+* [Vue面试题](docs/guide/vue.md)
+* [React面试题](docs/guide/react.md)
+
+### 框架原理
+
+* [虚拟DOM原理](docs/guide/virtualDom.md)
+* [Proxy比defineproperty优劣对比?](docs/guide/devsProxy.md)
+* [setState到底是异步的还是同步的?](docs/guide/setState.md)
+* [前端路由的实现](docs/guide/router.md)
+* [redux原理全解](docs/guide/redux.md)
+* [React Fiber 架构解析](docs/guide/fiber.md)
+* [React组件复用指南](docs/guide/abstract.md)
+* [React-hooks 抽象组件](docs/guide/reactHook.md)
+
+### UI组件
+
+* [如何搭建一个组件库的开发环境](docs/guide/componentCli.md)
+* [组件设计原则](docs/guide/component.md)
+* [实现轮播图组件](docs/guide/carousel.md)
+
+### 性能优化
+
+* [前端性能优化-加载篇](docs/guide/load.md)
+* [前端性能优化-执行篇](docs/guide/execute.md)
+
+### 工程化
+
+* [webpack面试题](docs/guide/webpack.md)
+* [前端工程化](docs/guide/engineering.md)
+
+### 工程化原理
+
+* [如何写一个babel](docs/guide/ast.md)
+* [Webpack HMR 原理解析](docs/guide/WebpackHMR.md)
+* [webpack插件编写](docs/guide/webpackPlugin.md)
+* [webpack 插件化设计](docs/guide/webpackPluginDesign.md)
+* [Webpack 模块机制](docs/guide/webpackMoudle.md)
+* [webpack loader实现](docs/guide/webpackLoader.md)
+* [如何开发Babel插件](docs/guide/babelPlugin.md)
+
+### 前端安全
+
+* [前端安全面试题](docs/guide/security.md)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,213 @@
+const { fs, path } = require('@vuepress/shared-utils')
+
+module.exports = ctx => ({
+  base: '/',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '前端进阶指南',
+      description: '可能是全网最给力的前端面试项目'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.svg` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  themeConfig: {
+    repo: 'xypisces/front-end-interview',
+    editLinks: true,
+    docsDir: 'packages/docs/docs',
+    locales: {
+      '/': {
+        editLinkText: '在 GitHub 上编辑此页',
+        nav: require('./nav/zh'),
+        sidebar: {
+            '/guide/': renderSiderBar()
+        }
+      }
+    }
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }],
+    ['@vuepress/medium-zoom', true],
+    ['@vuepress/google-analytics', {
+      ga: ''
+    }],
+    ['container', {
+      type: 'vue',
+      before: '<pre class="vue-container"><code>',
+      after: '</code></pre>',
+    }],
+    ['container', {
+      type: 'upgrade',
+      before: info => `<UpgradePath title="${info}">`,
+      after: '</UpgradePath>',
+    }],
+  ],
+  extraWatchFiles: [
+    '.vuepress/nav/zh.js',
+  ]
+})
+
+function renderSiderBar() {
+  return ([{
+      title: '前言',
+      collapsable: false,
+      children: [
+            '',
+            'preface'
+        ]
+  },
+  {
+    title: '面试技巧',
+    collapsable: false,
+    children: [
+          'resume',
+          'project',
+          'hr',
+      ]
+  },
+  {
+    title: '推荐',
+    collapsable: false,
+    children: [
+          'book'
+      ]
+  },
+  {
+      title: '前端基础',
+      collapsable: false,
+      children: [
+        'htmlBasic',
+        'cssBasic',
+        'jsBasic',
+        'browser',
+        'dom',
+        // 'designPatterns',
+      ]
+  },
+  {
+    title: '前端基础笔试',
+    collapsable: false,
+    children: [
+      'httpWritten',
+      'jsWritten',
+    ]
+},
+{
+  title: '前端原理详解',
+  collapsable: false,
+  children: [
+    'hoisting',
+    'eventLoop',
+    'immutable',
+    'memory',
+    'deepclone',
+    'event',
+    'mechanism',
+  ]
+},
+{
+  title: '计算机基础',
+  collapsable: true,
+  children: [
+        'http',
+        'tcp',
+    ]
+},
+{
+  title: '数据结构与算法',
+  collapsable: false,
+  children: [
+        'algorithm',
+        'string',
+    ]
+},
+{
+  title: '前端框架',
+  collapsable: false,
+  children: [
+        'framework',
+        'vue',
+        'react',
+    ]
+},
+{
+  title: '框架原理详解',
+  collapsable: true,
+  children: [
+        'virtualDom',
+        'devsProxy',
+        'setState',
+        'router',
+        'redux',
+        'fiber',
+        'abstract',
+        'reactHook',
+    ]
+},
+{
+  title: '框架实战技巧',
+  collapsable: true,
+  children: [
+        'componentCli',
+        'component',
+        'carousel',
+    ]
+},
+{
+  title: '性能优化',
+  collapsable: true,
+  children: [
+        'load',
+        'execute',
+    ]
+},
+{
+  title: '工程化',
+  collapsable: true,
+  children: [
+        'webpack',
+        'engineering',
+    ]
+},
+{
+  title: '工程化原理',
+  collapsable: true,
+  children: [
+        'ast',
+        'WebpackHMR',
+        'webpackPlugin',
+        'webpackPluginDesign',
+        'webpackMoudle',
+        'webpackLoader',
+        'babelPlugin',
+    ]
+},
+{
+  title: '安全',
+  collapsable: true,
+  children: [
+        'security',
+    ]
+},
+// {
+//   title: 'Node',
+//   collapsable: true,
+//   children: [
+//         'node',
+//     ]
+// },
+])
+}
